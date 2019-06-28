e39034d78d2c1db52a8d9fd3fe64182b034d78b2
bautie <36556238+bautie@users.noreply.github.com>
2019-06-28 22:17:00 +0900

    Fix num concat

--- cleanAlign.py
+++ cleanAlign.py
@@ -7,28 +7,28 @@ def cleanAlign():
 	
 	numDel = 0
 	srcFileName = "lm.json"
-	if 1 < len( sys.argv):
-		if 0 < len(sys.argv[1]):
-			srcFileName = sys.argv[1]
+	if 1 < len( sys.argv ):
+		if 0 < len( sys.argv[ 1 ] ):
+			srcFileName = sys.argv[ 1 ]
 	
 	srcFile = open( srcFileName )
 	srcJson = json.load( srcFile )
 	dstJson = {}
 
 	for fileName in srcJson:
-		print(fileName, end="" )
+		print( fileName, end="" )
 		faces = srcJson[ fileName ]
-		if 0 < len(faces):
+		if 0 < len( faces ):
 			dstJson[ fileName ] = faces
-			print( " -> copied")
+			print( " -> copied" )
 		else:
 			numDel += 1
-			print( " -> deleted " + numDel )
+			print( " -> deleted " + str( numDel ) )
 	
 	srcFile.close()
 	
-	dstFile = open( os.path.splitext( srcFileName )[ 0 ] + '_cleaned.json','w')
-	json.dump(dstJson,dstFile,indent=1)
+	dstFile = open( os.path.splitext( srcFileName )[ 0 ] + '_cleaned.json', 'w' )
+	json.dump( dstJson, dstFile, indent=1 )
 	dstFile.close()
 
 if __name__=='__main__':
